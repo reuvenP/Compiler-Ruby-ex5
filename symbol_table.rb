f866b969344d6007ebbfb0d2683bf39073242993
reuvenp <reuvenp@checkpoint.com>
2017-01-07 20:25:34 +0200

    symbol table - mid

--- symbol_table.rb
+++ symbol_table.rb
@@ -1,12 +1,15 @@
 class SymbolTable
   def initialize #Creates a new empty symbol table
-    @symbol_table = Hash.new
+    @symbol_table = Array.new
     @static_counter = 0
     @field_counter = 0
+    @subroutine_table = Array.new
+    @argument_counter = 0
+    @var_counter = 0
   end
 
   def start_subroutine #Stars a new subroutine scope (i.e. resets the subroutine's symbol table)
-    @subroutine_table = Hash.new
+    @subroutine_table = Array.new
     @argument_counter = 0
     @var_counter = 0
   end
@@ -15,13 +18,34 @@ class SymbolTable
   def define(name, type, kind) #Defines a new identifier for a given name, type and kind and assign it running index. STATIC and FIELD identifiers have a class scope, while ARG and VAR identifiers have a subroutine scope.
     case kind
       when 'static'
-        @symbol_table.push([@static_counter, ])
+        @symbol_table.push([name, type, kind, @static_counter])
+        @static_counter += 1
+      when 'field'
+        @symbol_table.push([name, type, kind, @field_counter])
+        @field_counter += 1
+      when 'arg'
+        @subroutine_table.push([name, type, kind, @argument_counter])
+        @argument_counter += 1
+      when 'var'
+        @subroutine_table.push([name, type, kind, @var_counter])
+        @var_counter += 1
+      else
     end
   end
 
   #params: kind: STATIC, FIELD ARG, VAR
   def var_count(kind) #Returns the number of variables of the given kind already defined in the current scope.
-
+    case kind
+      when 'static'
+        return @static_counter
+      when 'field'
+        return @field_counter
+      when 'arg'
+        return @argument_counter
+      when 'var'
+        return @var_counter
+      else
+    end
   end
 
 
